02f039ec5d5753aa26dce080a80b192baca0fedc
Pablo <soypab@gmail.com>
2018-11-10 19:02:12 +0100

    flume update

--- flume/priceEvolutionFlume.sh
+++ flume/priceEvolutionFlume.sh
@@ -1,1 +1,1 @@
-/usr/hdp/current/flume-server/bin/flume-ng agent -n transportProcess -f conf/priceEvolutionFlume.conf
+/usr/hdp/current/flume-server/bin/flume-ng agent -n priceEvolution -f conf/priceEvolutionFlume.conf
